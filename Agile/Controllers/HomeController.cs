f11cd66689ff820406e581d77bd8eb08085b38b5
Mathias <mathias.peters6@gmail.com>
2017-09-21 14:04:47 +0200

    Commit to test slack webhooks

--- Agile/Controllers/HomeController.cs
+++ Agile/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Agile.Controllers
 
 		public ActionResult About()
 		{
-			ViewBag.Message = "Your application description page.";
+			ViewBag.Message = "Your application description page is here.";
 
 			return View();
 		}
