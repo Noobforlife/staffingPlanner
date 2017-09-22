f7e5736b16c070f6c32a11af119a467e6cee8d4b
Korpen <mathias.peters6@gmail.com>
2017-09-22 13:48:28 +0200

    Changed names of topmost folders

---
+++ StaffingPlanner/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using StaffingPlanner.DAL;
+
+namespace StaffingPlanner.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult About()
+		{
+			var students = "";
+			ViewBag.Message = "Your application description page.";
+			ViewBag.Students = students;
+
+			return View(students);
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+	}
+}
---
+++ StaffingPlanner/DAL/StaffingPlanContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using StaffingPlanner.Models;
+
+namespace StaffingPlanner.DAL
+{
+	public class StaffingPlanContext : DbContext
+	{
+		public StaffingPlanContext() : base("StaffingPlanContext")
+		{
+		}
+
+		public DbSet<Course> Courses { get; set; }
+	}
+}
