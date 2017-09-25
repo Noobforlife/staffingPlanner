e3b12ab82c4dda0f1fcb073e2afaa9203da6edaa
Korpen <mathias.peters6@gmail.com>
2017-09-25 15:58:36 +0200

    Fixed build errors

--- StaffingPlanner/Models/TeacherCourseWorkload.cs
+++ StaffingPlanner/Models/TeacherCourseWorkload.cs
@@ -1,7 +1,10 @@
-﻿namespace StaffingPlanner.Models
+﻿using System;
+
+namespace StaffingPlanner.Models
 {
 	public class TeacherCourseWorkload
 	{
+		public Guid Id { get; set; }
 		public CourseEdition Course { get; set; }
 		public Teacher Teacher { get; set; }
 		public int Workload { get; set; }
