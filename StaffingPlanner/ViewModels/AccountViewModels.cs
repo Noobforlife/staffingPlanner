36baf683d703bd9f69d81cf943e94cb670744338
forsling <simon.forsling@gmail.com>
2017-09-25 14:43:55 +0200

    First iteration of login system

---
+++ StaffingPlanner/ViewModels/AccountViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StaffingPlanner.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        //[Required]
+        //[Display(Name = "Email")]
+        //[EmailAddress]
+        //public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        //[Display(Name = "Remember me?")]
+        //public bool RememberMe { get; set; }
+    }
+}
