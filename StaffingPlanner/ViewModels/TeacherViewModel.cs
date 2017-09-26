db4e109320ac912ab6cfa1d8d596d2b6a14c62d7
Korpen <mathias.peters6@gmail.com>
2017-09-26 11:36:21 +0200

    Added properties to TeacherViewModel

--- StaffingPlanner/ViewModels/TeacherViewModel.cs
+++ StaffingPlanner/ViewModels/TeacherViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace StaffingPlanner.ViewModels
+﻿using StaffingPlanner.Models;
+
+namespace StaffingPlanner.ViewModels
 {
 	public class TeacherViewModel
 	{
+		public string Name { get; set; }
+		public AcademicTitle Title { get; set; }
+		public int TotalHours { get; set; }
+		public int FallWork { get; set; }
+		public int SpringWork { get; set; }
+		public int RemainingHours { get; set; }
 	}
 }
