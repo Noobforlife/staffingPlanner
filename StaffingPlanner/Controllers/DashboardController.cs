cc06d09287e86ac779e82c385fa9f99b25149d5d
Korpen <mathias.peters6@gmail.com>
2017-09-29 12:24:29 +0200

    Added the dashboard controller and populated the dashboard page with basic data

--- StaffingPlanner/Controllers/DashboardController.cs
+++ StaffingPlanner/Controllers/DashboardController.cs
@@ -1,24 +1,36 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 using StaffingPlanner.DAL;
+using StaffingPlanner.Models;
+using StaffingPlanner.ViewModels;
 
 namespace StaffingPlanner.Controllers
 {
 	public class DashboardController : Controller
 	{
-        //public ActionResult About()
-        //{
-        //    var students = "";
-        //    ViewBag.Message = "Your application description page.";
-        //    ViewBag.Students = students;
-
-        //    return View(students);
-        //}
-
         public ActionResult Index()
         {
-            return View();
-        }
+	        var db = StaffingPlanContext.GetContext();
+	        var fallCourses = db.CourseOfferings.Where(c => c.TermYear.Term == Term.Fall)
+		        .Select(c => new DashboardViewModel
+		        {
+			        Code = c.Course.Code,
+			        Name = c.Course.Name,
+			        Periods = c.Periods
+		        }).ToList();
+	        var springCourses = db.CourseOfferings.Where(c => c.TermYear.Term == Term.Spring)
+				.Select(c => new DashboardViewModel
+		        {
+			        Code = c.Course.Code,
+			        Name = c.Course.Name,
+			        Periods = c.Periods
+		        }).ToList();
 
-    }
+			var courses = new Tuple<List<DashboardViewModel>, List<DashboardViewModel>>(fallCourses, springCourses);
+
+			return View(courses);
+        }
+	}
 }
---
+++ StaffingPlanner/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,82 @@
+﻿using StaffingPlanner.Models;
+
+namespace StaffingPlanner.ViewModels
+{
+	public class DashboardViewModel
+	{
+		public string Code { get; set; }
+		public string Name { get; set; }
+		public int PeriodsBefore { get; set; }
+		public int PeriodsDuration { get; set; }
+		public int PeriodsAfter { get; set; }
+
+		public Period Periods
+		{
+			set
+			{
+				PeriodsBefore = GetPeriodsBefore(value);
+				PeriodsDuration = GetPeriodsDuring(value);
+				PeriodsAfter = GetPeriodsAfter(value);
+			}
+		}
+
+		private static int GetPeriodsBefore(Period period)
+		{
+			switch (period)
+			{
+				case Period.P1:
+				case Period.P1P2:
+				case Period.AllPeriods:
+					return 0;
+				case Period.P2:
+					return 1;
+				case Period.P3:
+				case Period.P3P4:
+					return 2;
+				case Period.P4:
+					return 3;
+				default:
+					return -1;
+			}
+		}
+
+		private static int GetPeriodsDuring(Period period)
+		{
+			switch (period)
+			{
+				case Period.P1:
+				case Period.P2:
+				case Period.P3:
+				case Period.P4:
+					return 1;
+				case Period.P1P2:
+				case Period.P3P4:
+					return 2;
+				case Period.AllPeriods:
+					return 4;
+				default:
+					return -1;
+			}
+		}
+
+		private static int GetPeriodsAfter(Period period)
+		{
+			switch (period)
+			{
+				case Period.P1:
+					return 3;
+				case Period.P2:
+				case Period.P1P2:
+					return 2;
+				case Period.P3:
+					return 1;
+				case Period.P4:
+				case Period.P3P4:
+				case Period.AllPeriods:
+					return 0;
+				default:
+					return -0;
+			}
+		}
+	}
+}
