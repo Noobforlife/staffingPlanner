2a58ce8daf4322c55697b4362fce110e538b032d
andredonandre <andreowen10@gmail.com>
2017-10-05 15:30:10 +0200

    added frontend table filtering on the Teacher and course list. -Filtering is happening in a search input for now. - Also added pagination to the teacher and course tables

--- StaffingPlanner/Scripts/app.js
+++ StaffingPlanner/Scripts/app.js
@@ -1,20 +1,3 @@
 ﻿//Code to search for data in course table table
-function myFunction() {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("searchInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("courseListTable");
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
-            }
-        }
-    }
-}
-
 $('#courseListTable').DataTable();
+$('#teacherListTable').DataTable();
