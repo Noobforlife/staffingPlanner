4e90f35bb5b8240d84c71c4d948c09ac2465382d
Korpen <mathias.peters6@gmail.com>
2017-10-17 14:52:42 +0200

    Added model Comment

---
+++ StaffingPlanner/Models/Comment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StaffingPlanner.Models
+{
+	public class Comment
+	{
+		public Guid Id { get; set; }
+		public Guid ConnectedTo { get; set; }
+		public string Message { get; set; }
+	}
+}
