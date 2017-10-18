963949f50e069c8ca34d0dba7338916af78d9f0a
Korpen <mathias.peters6@gmail.com>
2017-10-18 11:10:58 +0200

    Made menu navigate to messages page only for DoS

--- StaffingPlanner/Controllers/DashboardController.cs
+++ StaffingPlanner/Controllers/DashboardController.cs
@@ -89,7 +89,7 @@ namespace StaffingPlanner.Controllers
 			        Code = c.Course.Code,
 			        Name = c.Course.Name,
 			        Periods = c.Periods,
-			        Status = c.CourseResponsible.Id == teacher.Id ? "info" : "success",
+			        Status = c.CourseResponsible.Id == teacher.Id ? "progress-bar-info" : "progress-bar-success",
 					State = c.State,
 			        CourseResponsible = c.CourseResponsible,
 		        })
---
+++ StaffingPlanner/Controllers/MessagesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using StaffingPlanner.DAL;
+
+namespace StaffingPlanner.Controllers
+{
+    public class MessagesController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+	        if (Globals.UserRole != Role.DirectorOfStudies)
+	        {
+		        try
+		        {
+			        Response.Redirect(Request.UrlReferrer.ToString());
+		        }
+		        catch
+		        {
+			        return RedirectToAction("Index", "Dashboard");
+		        }
+	        }
+            return View();
+        }
+    }
+}
